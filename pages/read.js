b256d4bfa4483bba2b6fc9d1b011524583d54a95
Reinoptland <reinoptland@gmail.com>
2022-11-22 11:14:54 +0100

    Fetch the cat on click, store in the state

--- pages/read.js
+++ pages/read.js
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-// 1. Find an API
+// 1. X Find an API
 // (Application Programming Interface -> Server where you can fetch data from)
 // https://cataas.com/cat?json=true
-// 2. We need to make a request (using fetch)
-// 3. We store the data from the api
+// 2. X We need to make a request (using fetch)
+// 3. X We store the data from the api
 // 4. We want to display the data to the user
 // 5. We want to make this request when the page loads (automatically)
 
 export default function Read() {
+  const [cat, setCat] = useState({});
+
+  async function fetchCat() {
+    console.log("FETCHING ...");
+    const response = await fetch("https://cataas.com/cat?json=true");
+    const json = await response.json();
+    console.log("RES", response);
+    console.log("JSON", json);
+    setCat(json);
+  }
+
+  console.log("CAT:", cat);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,6 +34,7 @@ export default function Read() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Fetching data</h1>
+        <button onClick={fetchCat}>GET A CAT!</button>
       </main>
     </div>
   );
